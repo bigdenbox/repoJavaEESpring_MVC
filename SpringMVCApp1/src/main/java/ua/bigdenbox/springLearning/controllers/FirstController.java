d197c494f82830d97a554f81d01b18d4ce125cb9
bigdenbox <bigdenbox@gmail.com>
2020-08-20 18:43:24 +0300

    Created annotations @RequestParam. Everything works fine

--- SpringMVCApp1/src/main/java/ua/bigdenbox/springLearning/controllers/FirstController.java
+++ SpringMVCApp1/src/main/java/ua/bigdenbox/springLearning/controllers/FirstController.java
@@ -7,16 +7,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 //@RequestMapping()
 public class FirstController {
-	
+
 	@GetMapping("/hello")
-	public String helloPage(@RequestParam("name") String name, @RequestParam("surname") String surname) {
-		System.out.println("helloPage:\nName " + name + " /nSurname " + surname);
+	public String helloPage(@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "surname", required = false) String surname) {
+		System.out.println("helloPage:\nName = " + name + " \nSurname = " + surname);
 		return "first/hello_page";
 	}
 
-	@GetMapping("goodbye")
-	public String goodbyePage(@RequestParam("name") String name, @RequestParam("surname") String surname) {
-		System.out.println("goodbyePage:\nName " + name + " /nSurname " + surname);
+	@GetMapping("/goodbye")
+	public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "surname", required = false) String surname) {
+		System.out.println("goodbyePage:\nName = " + name + " \nSurname = " + surname);
 		return "first/goodbye_page";
 	}
 }
